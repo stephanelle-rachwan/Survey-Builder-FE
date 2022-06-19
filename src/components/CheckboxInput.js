cedd45c44dc2b30842d4a03faa647d7bec371f60
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-06-19 21:01:12 +0300

    creating the CheckboxInput component

---
+++ src/components/CheckboxInput.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CheckboxInput = () => {
+  return (
+    <div>
+      <input className="checkbox" type="checkbox" value="Male" name="gender" />
+      Male <br />
+    </div>
+  );
+};
+
+export default CheckboxInput;
--- src/components/CheckboxQuestionBox.js
+++ src/components/CheckboxQuestionBox.js
@@ -1,35 +1,12 @@
 import React from "react";
+import CheckboxInput from "./CheckboxInput";
+import Question from "./Question";
 
 const CheckboxQuestionBox = () => {
   return (
     <p className="question">
-      <div className="question-txt">
-        <span className="colored">Question 2: </span> select multiple
-        checkboxes:
-      </div>
-      <div>
-        <input
-          className="checkbox"
-          type="checkbox"
-          value="Male"
-          name="gender"
-        />
-        Male <br />
-        <input
-          className="checkbox"
-          type="checkbox"
-          value="Female"
-          name="gender"
-        />
-        Female <br />
-        <input
-          className="checkbox"
-          type="checkbox"
-          value="Other"
-          name="gender"
-        />
-        Other
-      </div>
+      <Question />
+      <CheckboxInput />
     </p>
   );
 };
