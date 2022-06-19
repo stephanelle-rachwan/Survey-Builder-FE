94e358daa06bd5deb6afcd03db69b6496c13c043
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-06-19 18:45:23 +0300

    creating the AddQuestions component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from "./components/Login";
 import DisplaySurvey from "./components/DisplaySurvey";
 import SurveyQuestions from "./components/SurveyQuestions";
 import AddSurvey from "./components/AddSurvey";
+import AddQuestions from "./components/AddQuestions";
 
 function App() {
   return (
@@ -13,74 +14,7 @@ function App() {
       <DisplaySurvey />
       <SurveyQuestions />
       <AddSurvey />
-      <section className="add-questions">
-        <p className="filling">
-          <h1 className="question-txt">Survey Title:</h1>
-          <input
-            className="title-input"
-            type="text"
-            placeholder="Enter survey title"
-            required
-          ></input>
-        </p>
-        <p className="filling">
-          <h1 className="question-txt">How many questions do you want?</h1>
-          <input
-            className="number-input"
-            type="number"
-            placeholder="Enter # of questions"
-            required
-          ></input>
-        </p>
-        <p className="filling">
-          <h1 className="question-txt">Choose your question type: </h1>
-          <select className="type-input">
-            <option value="" disabled selected>
-              Select your question type
-            </option>
-            <option value="radio">Radio Button</option>
-            <option value="checkbox">Checkbox</option>
-            <option value="text">Text</option>
-            <option value="email">Email</option>
-            <option value="dropdown">Dropdown</option>
-            <option value="linear_scale">Linear Scale</option>
-          </select>
-        </p>
-        <p className="filling">
-          <h1 className="question-txt">Enter your question: </h1>
-          <input
-            className="question-input"
-            type="text"
-            placeholder="Question here"
-            required
-          ></input>
-        </p>
-        <p className="filling">
-          <h1 className="question-txt">
-            How many choices do you want your question to have?{" "}
-          </h1>
-          <input
-            className="numberChoices-input"
-            type="number"
-            placeholder="Enter # of choices"
-            required
-          ></input>
-        </p>
-        <p className="filling">
-          <h1 className="question-txt">Choices: </h1>
-          <input
-            className="choices-input"
-            type="text"
-            placeholder="Enter choice"
-            required
-          ></input>
-        </p>
-        <div className="flex-btn">
-          <button type="button" className="submit-btn">
-            Submit
-          </button>
-        </div>
-      </section>
+      <AddQuestions />
     </div>
   );
 }
